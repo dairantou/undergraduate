0edc82832c08504060e05d1338ecedc980923dc9
Yuki Shirahama <s1810339@gl.cc.uec.ac.jp>
2021-11-28 02:03:15 +0900

    mainimg2に変更

--- self-help.php
+++ self-help.php
@@ -99,7 +99,7 @@ foreach ($stmt as $row);
   
   	<!-- メイン画像 -->
 	<div id="mainBanner">
-			<img src="images/mainImg2.jpg" width="680" height="140" alt="">
+			<img src="images/mainimg2.jpg" width="680" height="140" alt="">
 
 		<div class="mobile_hide">
     	<div class="slogan">
