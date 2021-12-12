fbf2ba491ebc6b15f33f3601f8c14fb6f118dc90
Yuki Shirahama <s1810339@gl.cc.uec.ac.jp>
2021-12-12 17:19:29 +0900

    2

--- Simulationcp.php
+++ Simulationcp.php
@@ -143,12 +143,13 @@ foreach ($stmt as $row);
       <article>
 			
 			<p><br>家族構成、家庭内備蓄品の備蓄状況を入力することで、自身の備蓄状況について採点結果とフィードバックを得ることができます。<br><br>以下の17問の質問に答えて、自身の備蓄状況で自宅避難時に問題なく生活できるかどうかシミュレーションしてみましょう。<br><br>"入力を保存をする"というボタンを押すと、入力状況が保存されるため次回以降は変更箇所のみの入力で済みます。</p><br>
+			<p style="font-weight: bold;">Q1~Q6は半角数字で整数を入力してください</p><br>
 
 			<form action="Simulationcp-Register.php" method="post">
 			<dt>Q1.現在何人で暮らしていますか？</dt>
 			<dd>
 			<?php
-			echo "<input type='tel' id='q1' name='ans1' maxlength='4' style='text-align:right;width:50px;' value=".$row['ans1']."><label for='q1'>[人]</label><br>"
+			echo "<input type='tel' id='q1' name='ans1' maxlength='4' oninput='value = value.replace(/[０-９]/g,s => String.fromCharCode(s.charCodeAt(0) - 65248)).replace(/\D/g,'');' style='text-align:right;width:50px;' value=".$row['ans1']."><label for='q1'>[人]</label><br>"
 			?>
 			<br>
 			</dd>
--- simjs.js
+++ simjs.js
@@ -8,6 +8,13 @@ function saiten(name1,name2,name3,name4,name5,name6,name7,name8,name9,name10,nam
 	var radios5=parseFloat(document.getElementById(name5).value);
 	var radios6=parseFloat(document.getElementById(name6).value);
 
+	var a=document.search_form.q.value;
+  if(a=="")
+	if(radios1==0||radios1==""){
+		alert('Q1には1以上の数値を入力してください');
+		return;
+	}
+
 	//飲料水
 	var need=radios1*21;
 	var more=need-radios2;
